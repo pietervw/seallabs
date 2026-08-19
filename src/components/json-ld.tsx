8557e5c0f9943740f138f65ffaeb9dd0e2394901
Peter-Pan <pietervw@posteo.net>
2026-08-19 09:52:48 +0800

    feat: improve search representation

--- src/components/json-ld.tsx
+++ src/components/json-ld.tsx
@@ -9,7 +9,7 @@ export function JsonLd({ data }: JsonLdProps) {
     <script
       type="application/ld+json"
       dangerouslySetInnerHTML={{
-        __html: JSON.stringify(data),
+        __html: JSON.stringify(data).replace(/</g, "\\u003c"),
       }}
     />
   );
